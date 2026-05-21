da2bbf3c9b0344f0369d7cd7a3b5d11297472555
Matheus <matheus.infingardi@gmail.com>
2026-05-21 15:44:08 -0300

    chore: login/ register pages

---
+++ front-end/src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import AuthContainer from "@/components/auth/auth-container"
+
+export default function AuthPage() {
+  return (
+    <main className="min-h-screen bg-background">
+      <AuthContainer />
+    </main>
+  )
+}
---
+++ front-end/src/app/portal/page.tsx
@@ -0,0 +1,9 @@
+import PortalContainer from "@/components/auth/portal-container"
+
+export default function PortalPage() {
+  return (
+    <main className="min-h-screen bg-background">
+      <PortalContainer />
+    </main>
+  )
+}
